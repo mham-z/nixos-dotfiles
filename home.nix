15a155bdb0d40ccc8c743231bd1c68f6da99b0c1
Hamza <82454201+mham-z@users.noreply.github.com>
2026-07-26 23:32:51 +0500

    move foot terminal out and add padding

--- home.nix
+++ home.nix
@@ -25,6 +25,7 @@
 		./modules/tofi/tofi.nix
 		./modules/mako/mako.nix
 		./modules/fastfetch/fastfetch.nix
+		./modules/foot/foot.nix
 		./modules/autostart.nix
 	];
 
@@ -36,52 +37,6 @@
 		automount = true;
 		tray = "always";
 	};
-
-	programs.foot = {
-		enable = true;
-		
-		settings = {
-			main = {
-				font = "NotoSansM Nerd Font:size=12";
-				font-size-adjustment = 1;
-				pad = "0x0 center-when-maximized-and-fullscreen";
-				resize-by-cells = false;
-				resize-keep-grid = false;
-			};
-
-			bell = {
-				system = "yes";
-				urgent = "yes";
-				notify = "yes";
-				visual = "no";
-			};
-
-			# Rosé Pine Color Palette
-			colors-dark = {
-				background = "191724";
-				foreground = "e0def4";
-				regular0 = "26233a";
-				regular1 = "eb6f92";
-				regular2 = "9ccfd8";
-				regular3 = "f6c177";
-				regular4 = "31748f";
-				regular5 = "c4a7e7";
-				regular6 = "ebbcba";
-				regular7 = "e0def4";
-				bright0  = "47435d";
-				bright1  = "ff98ba";
-				bright2  = "c5f9ff";
-				bright3  = "ffeb9e";
-				bright4  = "5b9ab7";
-				bright5  = "eed0ff";
-				bright6  = "ffe5e3";
-				bright7  = "fefcff";
-				flash    = "f6c177";
-				cursor   = "191724 e0def4";
-			};
-		};
-	};
-
 
 	programs.git = {
 		enable = true;
