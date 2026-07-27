1d87adead047b5f9ef66897b2107abcdd083180b
Hamza <82454201+mham-z@users.noreply.github.com>
2026-07-28 00:02:44 +0500

    fix name

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
 	};
 
 	outputs = {self, nixpkgs, home-manager, vscode-insiders, ...}@inputs: {
-		nixosConfigurations.hamza = nixpkgs.lib.nixosSystem {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			specialArgs = {inherit inputs;}; 
 			modules = [
