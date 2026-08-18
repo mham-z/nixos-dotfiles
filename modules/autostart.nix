9caa7b293a3765650ae79155c7da7521322b326c
Hamza <82454201+mham-z@users.noreply.github.com>
2026-08-18 21:10:03 +0500

    easier to read

--- modules/autostart.nix
+++ modules/autostart.nix
@@ -16,11 +16,11 @@ let
 	};
 in {
 	systemd.user.services = {
-		zen = mkAutostart "Zen Browser" "flatpak run app.zen_browser.zen";
-		vesktop = mkAutostart "Equibop" "equibop";
-		zapzap = mkAutostart "ZapZap" "flatpak run com.rtosta.zapzap";
+		zen         = mkAutostart "Zen Browser" "flatpak run app.zen_browser.zen";
+		vesktop     = mkAutostart "Equibop"     "equibop";
+		zapzap      = mkAutostart "ZapZap"      "flatpak run com.rtosta.zapzap";
 		thunderbird = mkAutostart "Thunderbird" "thunderbird";
-		wlsunset = mkAutostart "wlsunset" "wlsunset -l 25 -L 67";
-		footserver = mkAutostart "Foot Server" "foot --server";
+		wlsunset    = mkAutostart "wlsunset"    "wlsunset -l 25 -L 67";
+		footserver  = mkAutostart "Foot Server" "foot --server";
 	};
 }
