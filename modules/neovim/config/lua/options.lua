94f289e2e03d8e7ccf9e9fdbf2e22156c7aad933
Hamza <82454201+mham-z@users.noreply.github.com>
2026-08-21 18:13:27 +0500

    more nvim config

--- modules/neovim/config/lua/options.lua
+++ modules/neovim/config/lua/options.lua
@@ -6,3 +6,21 @@ opt.mouse = "a"
 opt.clipboard = "unnamedplus"
 opt.wrap = false
 opt.cursorline = true
+opt.scrolloff = 10
+opt.list = true
+opt.confirm = true
+opt.ignorecase = true
+opt.smartcase = true
+
+vim.api.nvim_create_autocmd('UIEnter', {
+	callback = function()
+		vim.o.clipboard = 'unnamedplus'
+	end,
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
