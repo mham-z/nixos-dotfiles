7969c9cc7d9b1587edfa855a77c056a33b0ee82c
Hamza <82454201+mham-z@users.noreply.github.com>
2026-08-22 22:39:51 +0500

    edit the color scheme for better visibility

--- modules/neovim/config/lua/plugins/nord.lua
+++ modules/neovim/config/lua/plugins/nord.lua
@@ -1,1 +1,25 @@
 vim.cmd.colorscheme("nord")
+
+vim.api.nvim_set_hl(0, "Cursor", {
+	reverse = true;
+})
+
+vim.api.nvim_set_hl(0, "NormalFloat", {
+	bg = "#3b4252";
+	fg = "#d8dee9";
+})
+
+vim.api.nvim_set_hl(0, "FloatBorder", {
+	bg = "#3b4252";
+	fg = "#81a1c1";
+})
+
+vim.api.nvim_set_hl(0, "Normal", {
+	bg = "#333945";
+	fg = "#D8DEE9";
+})
+
+vim.api.nvim_set_hl(0, "NormalNC", {
+	bg = "#2E3440";
+	fg = "#D8DEE9";
+})
