dbfd33b2639cd85e5258df68fe3d7b41c0c4e19b
Hamza <82454201+mham-z@users.noreply.github.com>
2026-08-23 22:19:27 +0500

    neotree open on every new tab

--- modules/neovim/config/lua/plugins/neo-tree.lua
+++ modules/neovim/config/lua/plugins/neo-tree.lua
@@ -3,7 +3,22 @@ require("neo-tree").setup {
 		filtered_items = {
 			visible = true;
 		};
+		window = {
+			position = "left";
+			width = 40;
+		};
 	};
 }
+
+vim.api.nvim_create_autocmd("TabNew", {
+	callback = function()
+		vim.schedule(function()
+			require("neo-tree.command").execute {
+				action = "show";
+				source = "filesystem";
+			}
+		end)
+	end;
+})
 
 vim.keymap.set("n", "<leader>e",  "<cmd>Neotree toggle<CR>")
